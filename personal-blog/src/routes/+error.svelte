d3a55635b1b084ffd5d89cd515dd58339f911eb7
Jakub <kanshicz@gmail.com>
2025-04-07 13:09:03 +0200

    Add PocketBase dependency, update error page styling, and create blog page component

--- personal-blog/src/routes/+error.svelte
+++ personal-blog/src/routes/+error.svelte
@@ -7,7 +7,7 @@
 </svelte:head>
 
 <div class="text-center mt-12">
-    <h1 class="text-2xl text-red-500 font-bold">Error {page.status}</h1>
+    <h1 class="text-2xl">Error {page.status}</h1>
     <p class="mt-4 text-lg">{page.error.message}</p>
     <a href="/" class="text-blue-500 font-semibold hover:underline mt-6 inline-block">Go back to the homepage</a>
 </div>
---
+++ personal-blog/src/routes/blog/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+import PocketBase from 'pocketbase';
+
+const pb = new PocketBase('http://127.0.0.1:8090');
+let response="";
+
+async function getBlogData() {
+    response = await pb.collection('posts').getFullList({
+        sort: '+created',
+    });
+    console.log(response);
+}
+getBlogData();
+</script>
+
+<div class="container mx-auto w-[85%]">
+    <p>blog ty kokot</p>
+</div>
