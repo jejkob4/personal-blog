bd8d9b8216044d5d482ef678f517c0b4a9726c30
Jakub <kanshicz@gmail.com>
2025-04-07 19:03:05 +0200

    Refactor layout links for consistent hover styling and fix image source template in blog page

--- personal-blog/src/routes/+layout.svelte
+++ personal-blog/src/routes/+layout.svelte
@@ -6,13 +6,13 @@
 
 <div class="py-4 bg-gray-200">
 	<div class="flex justify-center items-center">
-		<a href="/" class="mx-2 decoration-2 decoration-blue-500 hover:underline hover:cursor-pointer">
+		<a href="/" class="mx-2 decoration-2 decoration-blue-500 hover:underline hover:text-blue-500 hover:cursor-pointer">
 			HOME
 		</a>
-		<a href="/blog" class="mx-2 decoration-2 decoration-blue-500 hover:underline hover:cursor-pointer">
+		<a href="/blog" class="mx-2 decoration-2 decoration-blue-500 hover:underline hover:text-blue-500 hover:cursor-pointer">
 			BLOG
 		</a>
-		<a href="/contact" class="mx-2 decoration-2 decoration-blue-500 hover:underline hover:cursor-pointer">
+		<a href="/contact" class="mx-2 decoration-2 decoration-blue-500 hover:underline hover:text-blue-500 hover:cursor-pointer">
 			CONTACT
 		</a>
 	</div>
--- personal-blog/src/routes/blog/[slug]/+page.svelte
+++ personal-blog/src/routes/blog/[slug]/+page.svelte
@@ -20,7 +20,7 @@
 {#if response}
     <div class="container mx-auto w-[85%] mt-16 min-h-screen">
         <p class="text-center text-gray-500 mt-2">{new Date(response.created).toLocaleString(undefined, { dateStyle: 'short', timeStyle: 'short' })}</p>
-        <img class="w-full h-[25rem] object-cover mt-6" src="{`${API_URL}/api/files/${response.collectionId}/${response.id}/${response.coverImg}`}" alt="coverImg">
+        <img class="w-full h-[25rem] object-cover mt-6" src="{API_URL}/api/files/{response.collectionId}/{response.id}/{response.coverImg}" alt="coverImg">
         <div class="mt-6 text-xl leading-relaxed text-gray-800">
             {@html response.content}
         </div>
